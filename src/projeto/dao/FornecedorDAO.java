2cf55778ca6aad3ff3e6b9b1aaefe7bc3526fdd0
Carlos Fernandes <itais1@hotmail.com>
2024-04-23 20:02:55 +0100

    Criamos O metodos ALTERAR E EXCLUIR FornecedoreDAO

--- src/projeto/dao/FornecedorDAO.java
+++ src/projeto/dao/FornecedorDAO.java
@@ -57,5 +57,66 @@ public class FornecedorDAO {
         }
 
     }
+     // excluir fornecedor
+
+    public void excluirFonecedores(Fornecedores obj) {
+        
+        try {
+            // 1 primeiro passo criar o comando sql
+            String sql = "delete from tb_fornecedores where id= ?";
+
+            // 2 passo conectar com o banco de dado e organizar o comamndo sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, obj.getId()); // isto colacar na primeira intorugacao o que vier dentro de atrebuto obj.jetNome
+            
+
+            // 3 passo Executar comando sql
+            stmt.execute();
+            stmt.close(); // para ele fechar a conexao apos a execucaoo
+            JOptionPane.showMessageDialog(null, "Ecluido com sucesso!");
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "ERRO" + erro);
+
+        }
     
+
+    }
+    
+     // alterar fornecedores
+    public void alterarFornecedores(Fornecedores obj) {
+        
+        try {
+            // 1 primeiro passo criar o comando sql
+            String sql = "update tb_clientes set nome=?,cnpj=?,email=?,telefone=?,celular=?"
+                    + ",cep=?,endereco=?,numero=?,complemento=?,bairro=?,cidade=?,estado=? where id=?";
+
+            // 2 passo conectar com o banco de dado e organizar o comamndo sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, obj.getNome()); // isto colacar na primeira intorugacao o que vier dentro de atrebuto obj.jetNome
+            stmt.setString(2, obj.getCnpj()); // isto colacar na primeira intorugacao o que vier dentro de atrebuto obj.jetNome
+          
+            stmt.setString(3, obj.getEmail());
+            stmt.setString(4, obj.getTelefone());
+            stmt.setString(5, obj.getTelemovel());
+            stmt.setString(6, obj.getCep());
+            stmt.setString(7, obj.getEndereco());
+            stmt.setInt(8, obj.getNumero());
+            stmt.setString(9, obj.getComplemento());
+            stmt.setString(10, obj.getBairro());
+            stmt.setString(11, obj.getCidade());
+            stmt.setString(12, obj.getUf());
+            stmt.setInt(13, obj.getId());
+
+            // 3 passo Executar comando sql
+            stmt.execute();
+            stmt.close(); // para ele fechar a conexao apos a execucaoo
+            JOptionPane.showMessageDialog(null, "Alterado  com sucesso!");
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "ERRO" + erro);
+
+        }
+
+    }
 }
+    
+
