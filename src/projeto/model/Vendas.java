79869c1773b512ee775558fa752022d90d2c35ba
Carlos Fernandes <itais1@hotmail.com>
2024-04-26 20:50:31 +0100

    Criando as classes Modelos de Vendas e ItensVenda

--- src/projeto/model/Vendas.java
+++ src/projeto/model/Vendas.java
@@ -1,13 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package projeto.model;
 
-/**
- *
- * @author itais
- */
+
 public class Vendas {
+    
+    
+    //atrebutos
+    private int id;
+    private Clientes cliente;
+    private String data_venda;
+    private double total_veda;
+    private String obs;
+    
+    
+    // gettrs e stters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Clientes getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Clientes cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getData_venda() {
+        return data_venda;
+    }
+
+    public void setData_venda(String data_venda) {
+        this.data_venda = data_venda;
+    }
+
+    public double getTotal_veda() {
+        return total_veda;
+    }
+
+    public void setTotal_veda(double total_veda) {
+        this.total_veda = total_veda;
+    }
+
+    public String getObs() {
+        return obs;
+    }
+
+    public void setObs(String obs) {
+        this.obs = obs;
+    }
+    
     
 }
